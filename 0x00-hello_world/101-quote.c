9bf9485e7d12c09639d6d4461f0a08212c2d410f
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-07 03:05:35 -0700

    my first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ * ssize_t write(int fd,const void *buf,size_t count);
+ *
+ * Return: 1 (error).
+ **/
+int main(void)
+{
+char qua[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, qua, 61);
+return (1);
+}
