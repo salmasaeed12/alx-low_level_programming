e8cd0110ad1d179e9e470ceeaa9d1ef3bac0cc4e
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-19 14:02:18 -0700

    f

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,12 @@ void rev_string(char *s)
 {
 int l, i;
 char temp;
+/* Calculate the length of the string*/
 for (l = 0; s[l] != '\0'; ++l)
+;
+
+/* Initialize i to 0 (starting from the beginning of the string)*/
+/* and l - 1 (starting from the end of the string)*/
 for (i = 0; i < l / 2; i++)
 {
 temp = s[i];
