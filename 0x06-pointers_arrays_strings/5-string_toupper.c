30a54ff6755fbf44f4d970b77a6a1c2870f3080d
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-20 19:05:08 -0700

    sss

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
  * @str: array of strings
  * Return: nothing
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i++)
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * *cap_string - a function that capitalizes all words of a string
+ * @c: array of string
+ * Return: nothing
+ */
+char *cap_string(char *)
+{
