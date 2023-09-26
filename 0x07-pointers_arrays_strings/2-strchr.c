3321e51e99be2feac85636a63087da5299c11bd5
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-25 17:41:11 -0700

    we are in deep shit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strchr - Locates a character in a string.
+ * @s: A pointer to the string to search.
+ * @c: The character to locate in the string.
+ * Return: A pointer to the first occurrence of the character @c
+ *  in the string @s, or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (NULL);
+}
