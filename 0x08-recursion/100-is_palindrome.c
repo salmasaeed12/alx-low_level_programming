09890f90bd6a4a1f761a7c7b1007b35fe81982b2
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-30 13:55:16 -0700

    fk

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,9 @@ int is_palindrome(char *s)
 {
 if (*s == '\0' || *(s + 1) == '\0')
 return (1);
-if (*s != s[strlen(s) - 1])
+int len = strlen(s);
+if (*s != s[len - 1])
 return (0);
+s[len - 1] = '\0';
 return (is_palindrome(s + 1));
 }
