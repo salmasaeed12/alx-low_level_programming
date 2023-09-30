4410e3b6bb72ebf0366825b026e7e3b77f7da592
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-30 16:03:25 -0700

    fk

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- *  _sqrt_recursion - Calculates the natural square root of a number.
- *   @n: The number for which to calculate the square root.
- *    Return: The natural square root of the number
- *             , or -1 if it doesn't have one.
+ * calculate_sqrt - Helper function for
+ *         binary search-like square root calculation.
+ * @n: The number for which to calculate the square root.
+ * @start: The start of the search range.
+ * @end: The end of the search range.
+ * Return: The natural square root of the number, or -1 if it doesn't have one.
  */
 int calculate_sqrt(int n, int start, int end);
 /**
-* calculate_sqrt - Helper function for binary
-*              search-like square root calculation.
-* @n: The number for which to calculate the square root.
-* @start: The start of the search range.
-* @end: The end of the search range.
-* Return: The natural square root of the number, or -1 if it doesn't have one.
-*/
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to calculate the square root.
+ * Return: The natural square root of the number, or -1 if it doesn't have one.
+ */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
@@ -29,6 +28,13 @@ else
 return (calculate_sqrt(n, 1, n / 2));
 }
 }
+/**
+ * calculate_sqrt - Helper function for binary
+ * @n: The number for which to calculate the square root.
+ * @start: The start of the search range.
+ * @end: The end of the search range.
+ *  Return: The natural square root of the number, or -1
+ */
 int calculate_sqrt(int n, int start, int end)
 {
 int mid = (start + end) / 2;
@@ -36,6 +42,10 @@ long long mid_squared = (long long)mid * mid;
 if (start > end)
 {
 return (-1);
+}
+if (mid_squared == n)
+{
+return (mid);
 }
 if (mid_squared == n)
 {
@@ -50,3 +60,4 @@ else
 return (calculate_sqrt(n, start, mid - 1));
 }
 }
+
