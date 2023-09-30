633310d1b366c66d4b8817ee7ea5cf6e3174b5fb
SalmaSaeed <salmasa3ed12@gmail.com>
2023-09-30 15:46:01 -0700

    fk

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,39 +1,39 @@
 #include "main.h"
 /**
- *  * is_prime_number - Checks if a number is prime.
- *   * @n: The number to check.
- *    *
- *     * Return: (1) if prime, (0) otherwise.
- *      */
+ * is_prime_recursive - Checks if a number is prime recursively.
+ * @n: The number to check.
+ * @i: The current divisor to check.
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_recursive(int n, int i);
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
 int is_prime_number(int n)
 {
-	    if (n <= 1) /* 0 and 1 are not prime numbers */
-		        {
-				        return (0);
-					    }
-
-	        return (is_prime_recursive(n, 2)); /* Start checking for primality from 2 */
-}
-
-/**
- *  * is_prime_recursive - Helper function to recursively check for primality.
- *   * @n: The number to check.
- *    * @divisor: The current divisor to check.
- *     *
- *      * Return: (1) if prime, (0) otherwise.
- *       */
-int is_prime_recursive(int n, int divisor)
+if (n <= 1)
 {
-	    if (divisor * divisor > n) /* If we've checked all divisors up to the square root */
-		        {
-				        return (1); /* It's a prime number */
-					    }
-
-	        if (n % divisor == 0) /* If n is divisible by the current divisor */
-			    {
-				            return (0); /* It's not a prime number */
-					        }
-
-		    return (is_prime_recursive(n, divisor + 1)); /* Check the next divisor */
+return (0);
 }
-
+return (is_prime_recursive(n, 2));
+}
+/**
+ * is_prime_recursive - Checks if a number is prime recursively.
+ * @n: The number to check.
+ * @i: The current divisor to check.
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_recursive(int n, int i)
+{
+if (i >= n)
+{
+return (1);
+}
+if (n % i == 0)
+{
+return (0);
+}
+return (is_prime_recursive(n, i + 1));
+}
