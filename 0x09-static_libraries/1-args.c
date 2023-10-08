6953bc5f0d6c0e8e5d111537444641711ab61b58
Salma Saeed <salmasa3ed12@gmail.com>
2023-10-07 19:30:14 -0700

    argc-argv

---
+++ 0x09-static_libraries/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: A C program that prints with put function
+ *     *@argc: arguments counter
+ *      *@argv: arguments vector
+ *       * Return: Always 0 (Success)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
