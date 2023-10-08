071bf33cbf3b7939b2e547c192c59c0f646d83e4
Salma Saeed <salmasa3ed12@gmail.com>
2023-10-08 15:54:37 -0700

    calloc task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ *  * malloc_checked -  function that allocates memory using malloc
+ *   *
+ *    * @b: size should be allocated
+ *     * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
