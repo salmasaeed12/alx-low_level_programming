f79278d3e5633be1f3aa28d5aba8495e354c27f0
SalmaSaeed <salmasa3ed12@gmail.com>
2023-10-07 18:28:33 -0700

    argc-argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints its name
+ * @argc: main argument count
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s/n", argv[0]);
+return (0);
+}
