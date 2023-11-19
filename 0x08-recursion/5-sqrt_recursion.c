b15ac34161c51f03529a26b6f30778d0d5efa9e7
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-18 21:01:15 -0800

    this argv and argc task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,23 +10,29 @@ int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-return (actual_sqrt_recursion(n, 0));
+helper(n, 0);
 }
-
 /**
- * actual_sqrt_recursion - Recursively calculates the natural square root
+ * helper - Recursively calculates the natural square root
  *                        of a non-negative number.
  * @n: The number for which to calculate the square root.
- * @i: An iterator to keep track of the current guess.
+ * @y: An iterator to keep track of the current guess.
  *
  * Return: The resulting square root, or -1 if no natural square root exists.
  */
-int actual_sqrt_recursion(int n, int i)
+int helper(int n, int y)
 {
-if (i * i > n)
+if (n == y * y)
+{
+return (y);
+}
+
+else if (y < n)
+{
+y++;
+helper(n, y++);
+}
+else
 return (-1);
-if (i * i == n)
-return (i);
-return (actual_sqrt_recursion(n, i + 1));
 }
 
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * @argc: The number of command line arguments
+ *    * @argv: An array containing the command line arguments
+ *     *
+ *      * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
