fee201bd24d13372f7c35f5bcd74e5e91b3b5d44
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-18 21:10:37 -0800

    this argv and argc task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * @argc: The number of command line arguments
+ *    * @argv: An array containing the command line arguments
+ *     *
+ *      * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
+
