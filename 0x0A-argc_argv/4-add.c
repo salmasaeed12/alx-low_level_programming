d23143618cd70dbc9871949643f77860cb0087ae
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-22 15:23:43 -0800

    0x0A-argc_argv task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
  *  * main - Entry point
  *   * @argc: The number of command line arguments
@@ -11,29 +11,23 @@
 int main(int argc, char *argv[])
 {
 int i = 1, m, sum = 0;
-
+char *ptr;
 if (argc == 1)
 {
 printf("0\n");
+return (0);
 }
-else
-{
 while (i < argc)
 {
-m = atoi(argv[i]);
-
-if (m >= 0 && m <= 1000000)
-{
-sum += m;
-i++;
-}
-else
+ptr = argv[i];
+if (!isdigit(*ptr))
 {
 printf("Error\n");
 return (1);
-break;
 }
-}
+m = atoi(argv[i]);
+sum += m;
+i++;
 }
 printf("%d\n", sum);
 return (0);
