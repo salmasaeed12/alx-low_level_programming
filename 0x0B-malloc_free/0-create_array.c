40f1f8bde073c0457898ffde0b5e2c8e266a5d65
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-21 18:26:53 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * Creates an array of characters and
- * initializes each element with the specified character.
- * Parameters:
- * - @size: The size of the array to create.
- * - @c: The character to initialize each element of the array.
- * Returns:
+/**
+ * create_array - create array of size size and assign char c.
+ * @size: The size of the array to create.
+ * @c: The character to initialize each element of the array.
+ * Return:
  * - A pointer to the dynamically allocated array.
  * - If size is 0 or if memory allocation fails, returns NULL.
  */
