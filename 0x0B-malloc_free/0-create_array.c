f361acd0930df1ebf102ba43b1aee5b6d043d772
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-21 18:13:31 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /*
  * Creates an array of characters and
  * initializes each element with the specified character.
@@ -11,10 +13,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-if (size == 0)
-return ('\0');
 char *ptr;
 unsigned int x = 0;
+if (size == 0)
+return ('\0');
 ptr = malloc(sizeof(c) * size);
 if (*ptr == '\0')
 return ('\0');
