489203d7f1bda41efa0c177c1f8ebb6f5af8cf0c
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-22 18:32:08 -0800

    0x0B-malloc_free task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,6 @@ char *str_concat(char *s1, char *s2)
 {
 char *ptr;
 int i = 0, m = 0, n = 0, l;
-if (s1 == NULL && s2 == NULL)
-return (NULL);
 while (s1[i] && s1 != NULL)
 i++;
 while (s2[n] && s2 != NULL)
