002d4d9a96a961e6c184f60f726fd18eacba71c1
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-11-30 16:28:51 -0800

    this pointer to function task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,13 +10,16 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
-if (size <= 0 && cmp == NULL && array == NULL)
+if (size <= 0)
 return (-1);
+if (array && cmp)
+{
 while (i < size)
 {
 if (cmp(array[i]))
 return (i);
 i++;
+}
 }
 return (-1);
 }
