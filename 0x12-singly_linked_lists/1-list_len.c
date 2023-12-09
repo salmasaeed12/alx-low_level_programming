733166cd6e3f689e9a4c515455194a4e41e02565
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-12-08 17:23:54 -0800

    _linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,12 +6,15 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t cot = 0;
+size_t cot = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
-		h = h->next;
-	cot++;
-	return (cot);
+if (h == NULL)
+return (0);
+while (h != NULL)
+{
+h = h->next;
+
+cot++;
+}
+return (cot);
 }
