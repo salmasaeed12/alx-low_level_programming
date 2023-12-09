386cafe73a2ddf2196120c5b2a90d147ec4fb3cc
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2023-12-08 17:13:28 -0800

    _linked_lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include"lists.h"
+/**
+ * list_len - print the data of the linked list
+ * @h: pointer to the first node of the list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t cot = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+		h = h->next;
+	cot++;
+	return (cot);
+}
